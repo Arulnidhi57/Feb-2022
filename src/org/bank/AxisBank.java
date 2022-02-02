11fd13b1ce3f62266d9d22187b5b50e511b7677c
Arulnidhi57 <arulnidhi557@gmail.com>
2022-02-02 09:27:21 +0530

    Arul

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -1,11 +1,17 @@
 package org.bank;
 
+import org.omg.CORBA.Current;
+
 public class AxisBank {
-public void saving() {
-	System.out.println("In my savings account i have one lakh rupees");
-}
-public void fixed() {
-	System.out.println("Minimum amount for fixed deposit id RS.50,000/-");
-	
-}
+	public void saving() {
+		System.out.println("In my savings account i have one lakh rupees");
+	}
+
+	public void fixed() {
+		System.out.println("Minimum amount for fixed deposit id RS.50,000/-");
+	}
+
+	public void current() {
+		System.out.println("I have no current account");
+	}
 }
